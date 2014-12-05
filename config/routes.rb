bb5366264b8648bdb4eb8f1ef762e1ae31563072
Danny <dannylshaw@yahoo.com>
2014-12-05 02:46:56 -0500

    added d3 and tooltips

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,8 @@ Dataproject::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => 'main#index'
 
+  match '/data-one', :to => redirect('/data.tsv')
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
