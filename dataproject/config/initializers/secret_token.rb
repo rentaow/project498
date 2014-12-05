075765478ec29f28358529d24a23150d6ce03f31
Danny <dannylshaw@yahoo.com>
2014-12-04 20:07:42 -0500

    restructuring directory

--- dataproject/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dataproject::Application.config.secret_token = '1fbb898f0173b2d08bed7543165ccfe312253680b5a7bdf360bdb234bc39dab3b8d81340ee2b1b78750f084c9188b9c37f691014d45b36b969731f030828ca5f'
--- dataproject/config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Dataproject::Application.config.session_store :cookie_store, key: '_dataproject_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Dataproject::Application.config.session_store :active_record_store
